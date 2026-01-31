cd250044a41adc819ef71060732a05ada45687c7
GokturKardas <gokturkardas@gmail.com>
2026-01-31 15:51:24 +0300

    ForDongusu dosyası yüklendi
    Java dilinden oluşan for döngüsü kodu yüklendi

---
+++ ForDongusu.java
@@ -0,0 +1,10 @@
+package JavaKodları;
+
+public class ForDongusu {
+   public static void main(String[] args) {
+      String metin = "Rakamımız: ";
+      for (int rakam = 1; rakam < 10;) {
+         System.out.println(metin + rakam);
+      }
+   }
+}
